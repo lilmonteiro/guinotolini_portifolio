ffa56d7d124e1df38c1afd29945f404debf8093f
LiandraMonteiroFCB <liandra.monteiro@fcb.com>
2023-08-21 19:15:29 -0300

    #D WSorking

--- src/components/Experience/Smile.jsx
+++ src/components/Experience/Smile.jsx
@@ -1,22 +1,21 @@
-
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
 export function Smile({ px, py, x, y, z, d, ...props }) {
   const { nodes, materials } = useGLTF("/models/smiley/smile-round.gltf");
-  const ref = useRef();
+  const refEyes = useRef();
+  const refSmile = useRef();
 
   useFrame((_, delta) => {
     // ref.current.rotation.x += 0.2 * delta;
-    ref.current.rotation.y += 0.5 * delta * d;
-    ref.current.rotation.x += 0.1 * delta * d;
+    // refEyes.current.rotation.x += 0.5 * delta * d;
+    // refSmile.current.rotation.x += 0.5 * delta * d;
+    // ref.current.rotation.x += 0.1 * delta * d;
   });
 
   return (
-
     <group
-      ref={ref}
       // {...props}
       // dispose={null}
       scale={[0.5, 0.5, 0.4]}
@@ -26,10 +25,12 @@ export function Smile({ px, py, x, y, z, d, ...props }) {
         geometry={nodes.smile_high002_fora001.geometry}
         material={nodes.smile_high002_fora001.material}
         position={[3.226, -2.292, 0.688]}
+        ref={refEyes}
       >
         <meshStandardMaterial attach="material" roughness={0.1} metalness={0.8} />
       </mesh>
       <mesh
+        ref={refSmile}
         geometry={nodes.smile_high002_fora.geometry}
         material={nodes.smile_high002_fora.material}
         position={[3.228, -2.29, 0]}
